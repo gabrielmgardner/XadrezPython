5bd503315d63304afb33e4d934bfcf1093cb73f2
gabrielgad <valdnato@gmail.com>
2025-09-18 00:07:35 -0300

    Create recurexpandir.py

---
+++ recurexpandir.py
@@ -0,0 +1,25 @@
+# Para adicionar posteriormente
+class RecursosAvancados:
+    def roque(self):
+        """Implementar roque"""
+        pass
+    
+    def en_passant(self):
+        """Implementar captura en passant"""
+        pass
+    
+    def promocao_peao(self):
+        """Implementar promoção de peão"""
+        pass
+    
+    def xeque(self):
+        """Implementar verificação de xeque"""
+        pass
+    
+    def xeque_mate(self):
+        """Implementar verificação de xeque-mate"""
+        pass
+    
+    def notacao_algebrica(self):
+        """Implementar notação algébrica completa"""
+        pass
